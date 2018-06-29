702d61328088c41d9156590531a20e39b01ce91f
KatherinePetrova <zetsu9@mail.ru>
2018-06-29 16:05:48 +0600

    wedding project

---
+++ guest/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Guest(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    invite = models.BooleanField(default=False)
+    
+    def first_name(self):
+        return self.user.first_name
+    
+    def last_name(self):
+        return self.user.last_name
+    
+    def email(self):
+        return sekf.user.email
+    
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
+    
+@receiver(post_save, sender=User)
+def create_user_asGuest(sender, instance, created, **kwargs):
+    if created:
+        Guest.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_asGuest(sender, instance, **kwargs):
+    instance.profile.save()
