6e363a6e7eddf44f38c528a150ed1031c76e04b2
KatherinePetrova <zetsu9@mail.ru>
2018-07-24 14:12:55 +0600

    slight changes

--- guest/static/scripts/script.js
+++ guest/static/scripts/script.js
@@ -1,19 +1,38 @@
 function lo(){
-    $("#site").scrollTop(0);
-    $("#welcome").fadeIn(1000);
-    $("#login").fadeIn(1500);
-    console.log(document.documentElement.clientWidth);
+    document.body.scrollTop = 0;
 }
 
 function logout() {
     localStorage.setItem("token","sddsds");
 }
 
+var menuToggle = true;
+var links = document.getElementsByClassName("link");
+function menu(){
+    if(menuToggle){
+        $("#fh").animate({width: "100%"}, 500);
+        for(var i=0; i<links.length; i++){
+            links[i].style = "display: block";
+        }
+        menuToggle = false;
+    } else {
+        close();
+    }
+}
+
+function close(){
+    $("#fh").animate({width: "0%"}, 500);
+    for(var i=0; i<links.length; i++){
+            links[i].style = "display: none";
+        }
+    menuToggle = true;
+}
+
 async function login(){
     let username = document.getElementById("name").value;
     let password = document.getElementById("password").value;
     
-    let response = await fetch('http://localhost:8000/api-token-auth/',
+    let response = await fetch('/api-token-auth/',
          {     method: 'POST',
                headers: {
                    "Content-Type": "Application/JSON",
@@ -28,18 +47,14 @@ async function login(){
     if (response.status !== 200) {
         alert("Incorrect username or password");
     } else {
-    
         let text = await response.text();
         let data = JSON.parse(text);
         console.log("Login ok!");
         localStorage.setItem("token", data.token);
-        $("#login").fadeOut(1500);
+        $("#login").fadeOut(500);
         document.getElementById("site").style.overflowY = "scroll";
-        $("#h_header").fadeIn(1500);
-        $("#welcome").animate({marginTop: "15%"}, 1500);
-        getInf();
-        
-        
+        $("#main_icon").fadeIn(500);
+        getInf(); 
     }
 }
 
@@ -74,5 +89,3 @@ function ido(){
     a.style.backgroundColor = "white";
     a.style.color = "black";
 }
-
-window.onload = lo;
