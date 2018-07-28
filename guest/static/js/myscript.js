4a1a1ba24915e8836420a24c45e984dce23838cb
KatherinePetrova <zetsu9@mail.ru>
2018-07-28 18:01:23 +0600

    screen changes

---
+++ guest/static/js/myscript.js
@@ -0,0 +1,11 @@
+function screens(){
+    var height = window.screen.availHeight + "px";
+    console.log(height);
+    $('#header').css("height", height);
+    $('#obr').css("height", height);
+    $('#invite').css("height", height);
+    $('#plan').css("height", height);
+    $('#contacts').css("height", height);
+}
+
+window.onload = screens;
